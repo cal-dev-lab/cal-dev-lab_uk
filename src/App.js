2beda4526b6c25773099088902f52ce7a7d64346
Callum Lees <hi@c-l.dev>
2022-08-25 13:35:21 +0100

    Installed React and Tailwind

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className='grid flex-col h-screen place-items-center bg-black'>
+      <h2 className='text-white text-6xl font-bold font-grotesk'>
+        cal-dev-lab
+        <br />
+        <span className='text-xl'>
+          Coming soon
+        </span>
+      </h2>
+      
+    </div>
+  );
+}
+
+export default App;
