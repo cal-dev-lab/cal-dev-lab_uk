03c0484636c85f17e2b150686afb7a92900dd0cc
Callum Lees <dev.cal.lees@gmail.com>
2022-09-06 21:45:21 +0100

    Added navbar and BS icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,37 @@
 <template>
-
+    <div>
+        <Navbar />
+        <nuxt />
+    </div>
 </template>
 
 <script>
+import Navbar from '~/components/Navbar.vue'
+
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'cal-dev-lab_uk',
-    htmlAttrs: {
-      lang: 'en'
+    head() {
+        return {
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for dad jokes.",
+                }
+            ],
+            link: [
+                // Dynamic canonical URL
+                {
+                    rel: "canonical",
+                    href: "https://cal-dev-lab.uk" + this.$route.path
+                },
+                // Bootstrap icons
+                {
+                    rel: "stylesheet",
+                    href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+                }
+            ],
+        }
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Front End Developer based in Cannock, Staffordshire. Visit me today to learn more about me.' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // Dynamic canonical URL
-      {
-        rel: "canonical",
-        href: "https://cal-dev-lab.uk" + this.$route.path
-      }
-    ]
-  },
+    components: { Navbar }
+}
 </script>
-
-<style>
-
-</style>
