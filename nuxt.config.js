8e93ee4d37821a01ade1dacde37087f77274f76c
Callum Lees <dev.cal.lees@gmail.com>
2022-09-06 21:45:30 +0100

    Updated SEO ttitle

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cal-dev-lab_uk',
+    title: 'CDL Design',
     htmlAttrs: {
       lang: 'en'
     },
